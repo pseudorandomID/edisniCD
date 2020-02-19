14bef0e7afb3f0ba9b1a3a8199df821f1695cf69
byeon <satoboy@naver.com>
2020-02-19 06:11:54 +0000

    headers

---
+++ dcHeaders.py
@@ -0,0 +1,18 @@
+
+class Headers:
+    galHeaders = {"Host": "nstatic.dcinside.com",
+                 "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0",
+                 "Accept": "text/css,*/*;q=0.1",
+                 "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3",
+                 "Accept-Encoding": "gzip, deflate, br",
+                 "Connection": "keep-alive"
+                }
+
+    commentHeaders = {"Host": "gall.dcinside.com",
+                      "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0",
+                      "Accept": "application/json, text/javascript, */*; q=0.01",
+                      "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3",
+                      "Accept-Encoding": "gzip, deflate, br",
+                      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                      "X-Requested-With": "XMLHttpRequest"
+                     }
--- main.py
+++ main.py
@@ -6,8 +6,13 @@ if __name__ == "__main__":
 
 
     pGalUrl = "https://gall.dcinside.com/board/lists/?id=programming"
+#    pGalUrl = "https://gall.dcinside.com/board/lists/?id=baseball_new8"
+#
     pGal = Gallery(url=pGalUrl, session=s)
 
+    print(pGal.posts)
+    print(pGal.postNums)
+    sendData = []
     for postNum in pGal.postNums:
         post = pGal.posts[postNum]
         print("글번호: " + post.num)
@@ -21,13 +26,31 @@ if __name__ == "__main__":
         print("조회수: " + post.count)
         print("추천: " + post.recommend)
         print("url: " + post.url)
-        read = Post(post.url)
-        print(read.content)
-        print(read.comments)
+        #read = Post(post.url)
+        #print(read.content)
+        #print(read.comments)
         print("")
+        writer = ""
 
+        if post.ip == "":
+            writer += post.nick + "(" + post.uid + ")"
+        else:
+            writer += post.ip
+        sendText = ""
+        sendText += "글번호: %s\n" % post.num
+        sendText += "제목: %s\n" % post.title
+        sendText += "작성자: %s\n" % writer
+        sendText += "날짜: %s\n" % post.date
+        sendText += "조회수: %s\n" % post.count
+        sendText += "추천: %s\n" % post.recommend 
+        #sendText += "url: %s\n" % post.url 
+        sendData.insert(0, sendText)
     commentData={"id":"programming","no":"1261736","cmt_id":"programming",
                  "cmt_no":"1261736", "e_s_n_o":"3eabc219ebdd65f53e",
                  "comment_page":"1","sort":"sort","prevCnt":"","board_type":""}
     #post = Post("https://gall.dcinside.com/board/view/?id=programming&no=1261729&page=1", s)
     #print(post.comments)
+    for data in sendData:
+        params = {"chat_id":"1020133801", "text":data }
+        s.get("https://api.telegram.org/bot1048341657:AAFSB6dGOLlZRcxwcaqeXNejDhYLs7T8HAA/sendMessage", params=params)
+
